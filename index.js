342476381c95c70eaa78b968572d4f8c919550b3
github username <github email address>
2019-08-21 21:27:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = block => Math.abs(block - 42);
+
+const distanceFromHqInFeet = block => distanceFromHqInBlocks(block) * 264;
+
+const distanceTravelledInFeet = (block1, block2) => Math.abs(block2 - block1) * 264;
+
+const calculatesFarePrice = (start, destination) => {
+  let distance = Math.abs(destination - start) * 264;
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 2500) {
+    return 'cannot travel that far';
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return (distance - 400) * 0.02;
+  }
+}
